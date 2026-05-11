e13ba17a96130921708f4bfbe3ab1ad47d5e2206
krl0s <delcastillokrlos@gmail.com>
2026-05-11 08:23:36 -0500

    SEXO

---
+++ src/core/nodo/Nodo.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package core.nodo;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author krl0s
+ */
+public abstract class Nodo {
+    protected Nodo sig = null;
+    protected HashMap<String, String> info;    
+
+    public Nodo(HashMap info) {
+        this.info = info;
+    }
+
+    public Nodo getSig() {
+        return sig;
+    }
+
+    public void setSig(Nodo sig) {
+        this.sig = sig;
+    }
+
+    public Nodo getInfo() {
+        return info;
+    }
+
+    public void setInfo(HashMap info) {
+        this.info = info;
+    }
+    
+    
+}
